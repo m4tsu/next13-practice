04ea2156165486e1da47dbb60c122d7ce1b04146
m4tsu <matsurec0147@gmail.com>
2022-11-14 03:15:03 +0900

    feat: base layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,27 @@
-import "@/styles/globals.css";
+import '@/styles/globals.css';
+import { ReactNode } from 'react';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <body>
+        <div className="h-screen flex flex-col">
+          <header className="border-b-gray-400 border-b-2">
+            <div className="container mx-auto py-2">
+              <h1 className="text-3xl font-bold">Header!!!</h1>
+            </div>
+          </header>
+          <main className="flex-1 container mx-auto py-4">{children}</main>
+          <footer className="border-t-gray-400 border-t-2">
+            <div className="container mx-auto py-2">
+              <h1 className="text-xl font-bold">Footer!!!</h1>
+            </div>
+          </footer>
+        </div>
+      </body>
     </html>
   );
-}
+};
+
+export default RootLayout;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import { NextPage } from "next";
+import { NextPage } from 'next';
 
 const TopPage: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Next13, TopPage!</h1>;
+  return <h1 className="text-3xl">TopPage!</h1>;
 };
 
 export default TopPage;
